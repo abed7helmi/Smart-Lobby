2db577fb172b901dd543c1994fa6a2fa1cf58859
Anas117 <anas.altundag@gmail.com>
2021-01-29 16:34:45 +0100

    "rename serv to server"

--- backend-serv/src/main/java/episen/si/ing1/pds/backend/server/BackendServer.java
+++ backend-serv/src/main/java/episen/si/ing1/pds/backend/server/BackendServer.java
@@ -1,10 +1,10 @@
-package episen.si.ing1.pds.backend.serv;
+package episen.si.ing1.pds.backend.server;
 
 
 /*import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;*/
 
-public class BackendServ {
+public class BackendServer {
     /*private final static Logger logger = LoggerFactory.getLogger(BackendServ.class.getName());*/
     public static void main(String[] args) {
         /*logger.info("backend marche");*/
