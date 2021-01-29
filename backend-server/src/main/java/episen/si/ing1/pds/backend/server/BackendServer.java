3fc2d7182107279e7a3454f451a055c184847c77
Anas117 <anas.altundag@gmail.com>
2021-01-29 18:13:29 +0100

    "logger update"

--- backend-server/src/main/java/episen/si/ing1/pds/backend/server/BackendServer.java
+++ backend-server/src/main/java/episen/si/ing1/pds/backend/server/BackendServer.java
@@ -3,11 +3,15 @@ package episen.si.ing1.pds.backend.server;
 
 /*import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;*/
+import java.util.logging.Logger;
 
 public class BackendServer {
-    /*private final static Logger logger = LoggerFactory.getLogger(BackendServ.class.getName());*/
+	
+    /*private final static Logger logger = LoggerFactory.getLogger(BackendServer.class.getName());*/
+	private final static Logger logserv = Logger.getLogger("Server Logger");
+	
     public static void main(String[] args) {
         /*logger.info("backend marche");*/
-    	System.out.println("server working");
+    	logserv.info("server working");
     }
 }
--- client/src/main/java/episen/si/ing1/pds/client/Client.java
+++ client/src/main/java/episen/si/ing1/pds/client/Client.java
@@ -1,8 +1,12 @@
 package episen.si.ing1.pds.client;
 
+import java.util.logging.Logger;
+
 public class Client {
 
+	private final static Logger logclient = Logger.getLogger("Client Logger");
+	
     public static void main(String[] args) {
-        System.out.println("client working");
+        logclient.info("client working");
     }
 }
