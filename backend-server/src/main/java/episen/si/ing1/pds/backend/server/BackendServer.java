a2abd4849e7148ae5c3cc9846dd1f0353eaed7b0
Anas117 <anas.altundag@gmail.com>
2021-01-29 16:40:26 +0100

    "rename 3"

---
+++ backend-server/src/main/java/episen/si/ing1/pds/backend/server/BackendServer.java
@@ -0,0 +1,13 @@
+package episen.si.ing1.pds.backend.server;
+
+
+/*import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;*/
+
+public class BackendServer {
+    /*private final static Logger logger = LoggerFactory.getLogger(BackendServ.class.getName());*/
+    public static void main(String[] args) {
+        /*logger.info("backend marche");*/
+    	System.out.println("server working");
+    }
+}
---
+++ backend-server/src/main/scripts/backend-server.bat
@@ -0,0 +1,4 @@
+cd ..
+cd java\episen\si\ing1\pds\backend\serv
+javac BackendServ.java
+java BackendServ.java
