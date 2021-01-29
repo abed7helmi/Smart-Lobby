7fbcc5a869f8843cd03b86a83845e36812ae1a97
Helmi Abed <abed.helmi@yahoo.fr>
2021-01-29 14:46:34 +0100

    test logger

--- backend-serv/src/main/java/episen/si/ing1/pds/backend/serv/BackendServ.java
+++ backend-serv/src/main/java/episen/si/ing1/pds/backend/serv/BackendServ.java
@@ -1,8 +1,12 @@
 package episen.si.ing1.pds.backend.serv;
 
-public class BackendServ {
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BackendServ {
+    private final static Logger logger = LoggerFactory.getLogger(BackendServ.class.getName());
     public static void main(String[] args) {
-        System.out.print("keke");
+        logger.info("backend marche");
     }
 }
