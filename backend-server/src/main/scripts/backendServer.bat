844ca253a1b01560b33da7a4764bddb9c371a46b
cedric1011 <31357068+cedric1011@users.noreply.github.com>
2021-02-04 20:41:04 +0100

    Delete backendServer.bat

--- backend-server/src/main/scripts/backendServer.bat
+++
@@ -1,3 +0,0 @@
-set m2=%M2_REPO%
-set clsspth=%m2%\episen\si\ing1\pds\backend-server\1.0-SNAPSHOT\backend-server-1.0-SNAPSHOT-jar-with-dependencies.jar
-call java -cp %clsspth% episen.si.ing1.pds.backend.server.BackendServer%*
