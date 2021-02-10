a3575bbbba097c30958c1b527dd19fb2d4e8f2ce
cedric1011 <cedric6701827@gmail.com>
2021-02-10 19:21:50 +0100

    properties file and numeric args  backendServer

--- backend-server/src/main/java/episen/si/ing1/pds/backend/server/BackendServer.java
+++ backend-server/src/main/java/episen/si/ing1/pds/backend/server/BackendServer.java
@@ -1,7 +1,12 @@
+
 package episen.si.ing1.pds.backend.server;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.apache.commons.cli.*;
+
+import java.io.InputStream;
+import java.util.Properties;
 
 public class BackendServer {
 
@@ -9,15 +14,38 @@ public class BackendServer {
 
     public static void main(String[] args) throws Exception {
         final Options options = new Options();
-        final CommandLineParser clp = new DefaultParser();
+
         final Option testmode = Option.builder().longOpt("testmode").build();
+        final Option numberConnection = Option.builder().longOpt("numberConnection").hasArg().argName("numberConnection").build();
+
         options.addOption(testmode);
-        final CommandLine commandLine = clp.parse(options, args) ;
+        options.addOption(numberConnection);
+
+
+        final CommandLineParser clp = new DefaultParser();
+        final CommandLine commandLine = clp.parse(options, args);
+
         boolean testmodeV = false;
-        if (
-                commandLine.hasOption("testmode")
-        )
-            testmodeV = true;
-        logger.info("BackendServer is running with testmode = {}",testmodeV);
+
+        InputStream inStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("properties");
+        Properties props = new Properties();
+        try {
+            props.load(inStream);
+            int numberConnectionV = Integer.parseInt(props.getProperty("server.numberConnectionV"));
+            if (commandLine.hasOption("testmode")) {
+                testmodeV = true;
+            }
+
+            if (commandLine.hasOption("numberConnection")) {
+                testmodeV = true;
+                numberConnectionV = Integer.parseInt(commandLine.getOptionValue("numberConnection"));
+            }
+
+            logger.info("BackendServer is running with (testmode = {}), numberConnection = {}.",
+                    testmodeV, numberConnectionV);
+
+        } catch (Exception e) {
+            logger.info(e.getMessage());
+        }
     }
 }
