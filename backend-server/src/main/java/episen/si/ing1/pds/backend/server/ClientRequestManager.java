21fe2d78e2961299e9a8822c1206b0e24073a446
Anas117 <anas.altundag@gmail.com>
2021-03-19 16:23:51 +0100

    ClientRequestManager thread modification

--- backend-server/src/main/java/episen/si/ing1/pds/backend/server/ClientRequestManager.java
+++ backend-server/src/main/java/episen/si/ing1/pds/backend/server/ClientRequestManager.java
@@ -12,38 +12,42 @@ import java.sql.SQLException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ClientRequestManager implements Runnable{
+public class ClientRequestManager {
 
 	private final static Logger logger = LoggerFactory.getLogger(ClientRequestManager.class);
 	private final PrintWriter output;
 	private final BufferedReader input;
-	private final static String name="client-thread-manager";
-	private Thread self;
 	private Connection c;
-	
-	public ClientRequestManager(Socket socket, Connection connection) throws IOException {
+	private String name = "client-thread-manager";
+	private Thread self;
+
+	public ClientRequestManager(Socket socket, Connection connection) throws IOException, SQLException {
+		c = connection;
 		output = new PrintWriter(socket.getOutputStream(),true);
 		input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		self = new Thread(name);
-		self.run();
-		c = connection;
+		self = new Thread(name) {
+			@Override
+			public void run() {
+				try {
+					logger.info(input.readLine());
+					ResultSet rs = c.createStatement().executeQuery("select * from test");
+					StringBuilder sb = new StringBuilder();
+					while (rs.next()) {
+						sb.append("id=" + rs.getInt(1) + "|name=" + rs.getString(2) + "\n");
+					}
+					logger.info(sb.toString());
+					output.write("done");
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		self.start();
 	}
 	
-	@Override
-	public void run() {
-		try {
-			logger.info(input.readLine());
-			ResultSet rs = c.createStatement().executeQuery("select * from test");
-			StringBuilder sb = new StringBuilder();
-			while (rs.next()) {
-				sb.append("id=" + rs.getInt(1) + "|name=" + rs.getString(2) + "\n");
-			}
-			output.print(sb.toString());
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+	public Thread getSelf() {
+		return self;
 	}
-
 }
--- backend-server/src/main/java/episen/si/ing1/pds/backend/server/ServerCore.java
+++ backend-server/src/main/java/episen/si/ing1/pds/backend/server/ServerCore.java
@@ -5,6 +5,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,22 +14,25 @@ public class ServerCore {
 
 	private ServerSocket serverSocket;
 	private final static Logger logger = LoggerFactory.getLogger(ServerCore.class);
-	
-	
+
 	public ServerCore(final ServerConfig config) throws IOException {
 		serverSocket = new ServerSocket(config.getConfig().getListenPort());
 		serverSocket.setSoTimeout(config.getConfig().getTimeOut());
 	}
-	
-	public void serve(DataSource d) throws IOException {
-		try {
-			final Socket socket = serverSocket.accept();
-			logger.debug("Request received");
-			Connection connection = d.send();
-			logger.debug("Connection established");
-			final ClientRequestManager clientRequestManager = new ClientRequestManager(socket, connection);
-		}catch (SocketTimeoutException e) {
-			logger.debug("Request timeout");
+
+	public void serve(DataSource d) throws IOException, SQLException {
+		while (true) {
+			Connection c = d.send();
+			try {
+				final Socket socket = serverSocket.accept();
+				logger.debug("Request received");
+				final ClientRequestManager clientRequestManager = new ClientRequestManager(socket, c);
+				clientRequestManager.getSelf().join();;
+			} catch (SocketTimeoutException | InterruptedException e) {
+				logger.debug("Request timeout");
+			} finally {
+				d.receive(c);
+			}
 		}
 	}
 }
